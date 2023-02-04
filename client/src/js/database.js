0ad27a13a64e0634acbb10f8b0dca7494f2a223f
abdulrfarag <ar060290@gmail.com>
2023-02-04 00:08:37 -0600

    compeleted project and added README

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -19,7 +19,7 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate')
   const request = store.put({id: 1, value: content})
   const result = await request;
-  console.log('data saved', result.value)
+  console.log('data saved', result)
 }
 
 // TODO: Add logic for a method that gets all the content from the database
